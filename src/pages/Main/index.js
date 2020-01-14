f3fbfe271363446cb2a324fc3488745be4f7c532
eltonlazzarin <elton.lazzarin@outlook.com>
2020-01-13 23:03:27 -0300

    translated page

--- src/pages/Main/index.js
+++ src/pages/Main/index.js
@@ -5,23 +5,28 @@ import { Container, Banner, HitsSection, Content } from './styles';
 import Top10List from '../../components/Top10List';
 
 export default function Main() {
-  const [placeholderValue, setPlaceholderValue] = useState('https://google.com');
-  const [buttonValue, setButtonValue] = useState('Encurtar');
+  const [placeholderValue, setPlaceholderValue] = useState(
+    'https://google.com'
+  );
+  const [buttonValue, setButtonValue] = useState('Shorten');
 
   const handleFormSubmit = e => {
-    e.preventDefault()
-  }
+    e.preventDefault();
+  };
 
   const handleChangeValues = () => {
-    setPlaceholderValue('http://chr.dc/xyzxyz')
-    setButtonValue('Copiar')
-  }
+    setPlaceholderValue('http://chr.dc/xyzxyz');
+    setButtonValue('Copy');
+  };
 
   return (
     <Container>
       <Banner>
-        <h2>Encurte seus links.</h2>
-        <p>Links são longos. Encurte os links que você deseja compartilhar, e acompanhe enquanto viajam através da internet.</p>
+        <h2>Shorten your links.</h2>
+        <p>
+          Links are long. Shorten the links you want to share, and follow along
+          as they travel over the internet.
+        </p>
         <form onSubmit={handleFormSubmit}>
           <input type="text" placeholder={placeholderValue} />
           <button onClick={handleChangeValues}>{buttonValue}</button>
@@ -32,7 +37,7 @@ export default function Main() {
         <Content>
           <h3>HITS</h3>
           <span>35.713.571</span>
-          <p>Cliques em links</p>
+          <p>Clicks on links</p>
         </Content>
       </HitsSection>
     </Container>
